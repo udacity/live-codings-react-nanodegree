f199138fa0b570e486aa3e2e6f4e7149a6b68b2c
João Eduardo Ferreira Bertacchi <joaobertacchi@gmail.com>
2018-12-21 11:27:11 -0200

    refactor: Test If component with no hack

--- react-fundamentals/2-iniciando-com-testes-em-react-solution/solution/src/If.test.js
+++ react-fundamentals/2-iniciando-com-testes-em-react-solution/solution/src/If.test.js
@@ -2,17 +2,16 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import If from './If';
 
-// Beware: this is a very hacky test suite.
 describe('[Component] If', () => {
-  const children = 'hello, world!'
+  const children = <p>Hello, World!</p>;
 
   it('renders null if test is false', () => {
     const wrapper = shallow(<If test={false} children={children} />);
-    expect(wrapper.debug()).toBe("");
+    expect(wrapper.getElement()).toEqual(null);
   });
 
   it('renders children if test is true', () => {
     const wrapper = shallow(<If test={true} children={children} />);
-    expect(wrapper.debug()).toEqual(children);
+    expect(wrapper.getElement()).toEqual(children);
   });
 });
